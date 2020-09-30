a3eafd0143956e5fde18b5347688c41e7046041d
JamesLeggero <james.m.leggero@gmail.com>
2020-09-29 21:26:24 -0400

    config folder made

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001
 const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
+const passport = require('./config/passport')
 
 const MONGO_URI = process.env.MONGO_URI
 const db = mongoose.connection
@@ -17,6 +18,12 @@ mongoose.connect(MONGO_URI, {
 db.on('open', () => {
     console.log('TALK')
 })
+app.use(cors())
+app.use(express.json())
+app.use(passport.initialize())
+
+app.use('/api/roasters', require('./controllers/roasters.js'))
+app.use('/api/farmers', require('./controllers/farmers.js'))
 
 app.listen(PORT, () => {
     console.log(`COFFEE`)
