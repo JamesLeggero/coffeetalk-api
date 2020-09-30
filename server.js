dc273fbe6b8ab8b80c1c098a5eac75ea15fb97ae
JamesLeggero <james.m.leggero@gmail.com>
2020-09-30 16:43:56 -0400

    socket creating room on LH with server.listen, no 404

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ require('dotenv').config()
 const passport = require('./config/passport')()
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3001
 const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
+const http = require('http')
+const server = http.createServer(app)
+const socket = require('socket.io')
+const io = socket(server)
 
+const PORT = process.env.PORT || 3001
 const MONGO_URI = process.env.MONGO_URI
 const db = mongoose.connection
 
@@ -27,6 +31,40 @@ const roasterController = require('./controllers/roasters.js')
 app.use('/roasters', roasterController)
 app.use('/farmers', require('./controllers/farmers.js'))
 
-app.listen(PORT, () => {
-    console.log(`COFFEE`)
+const rooms = {}
+
+io.on("connection", socket => {
+    socket.on("join room", roomID => {
+        if (rooms[roomID]) {
+            rooms[roomID].push(socket.id);
+        } else {
+            rooms[roomID] = [socket.id];
+        }
+        const otherUser = rooms[roomID].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id);
+        }
+    });
+
+    socket.on("offer", payload => {
+        io.to(payload.target).emit("offer", payload);
+    });
+
+    socket.on("answer", payload => {
+        io.to(payload.target).emit("answer", payload);
+    });
+
+    socket.on("ice-candidate", incoming => {
+        io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+    });
+});
+
+// app.listen(PORT, () => {
+//     console.log(`COFFEE`)
+// })
+
+
+server.listen(PORT, () => {
+    console.log('ROOMY')
 })
