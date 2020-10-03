857e5bbdc4ba7974381b295be463feb53cea761f
JamesLeggero <james.m.leggero@gmail.com>
2020-10-02 20:32:32 -0400

    added :id get to roaster

--- controllers/roasters.js
+++ controllers/roasters.js
@@ -6,87 +6,98 @@ const passport = require('../config/passport.js')
 const config = require('../config/config.js')
 const Roaster = require('../models/roaster.js')
 
-router.get('/', (req,res) => {
-    Roaster.find({}, (error, allRoasters) => {
-        error ? res.status(404).json(error) :
-        res.status(200).json(allRoasters)
-    })
+router.get('/', (req, res) => {
+  Roaster.find({}, (error, allRoasters) => {
+    error ? res.status(404).json(error) :
+      res.status(200).json(allRoasters)
+  })
 })
 
 
 
 router.post("/signup", (req, res) => {
-    console.log(req.body);
-    if (req.body.username && req.body.password) {
-    
-      // Hash the password:
-      req.body.password = bcrypt.hashSync(
-        req.body.password,
-        bcrypt.genSaltSync(10)
-      );
-  
-      Roaster.findOne({ username: req.body.username }, (roaster) => {
-        console.log("========findOne=======", roaster);
-        if (!roaster) {
-          console.log("Running create roaster");
-          Roaster.create(req.body, (error, createdRoaster) => {
-            console.log("createdRoaster", createdRoaster);
-            console.log("error", error);
-            if (createdRoaster) {
-              let payload = {
-                id: createdRoaster.id,
-              };
-              console.log(payload);
-              let token = jwt.encode(payload, config.jwtSecret);
-              console.log(token);
-              res.json({
-                token: token,
-              });
-            } else {
-              console.log("failed to create roaster");
-              res.sendStatus(401);
-            }
-          });
-        } else {
-          console.log("Roaster already exists, try logging in instead");
-          res.sendStatus(401);
-        }
-      });
-    } else {
-      res.sendStatus(401);
-    }
-  });
+  console.log(req.body);
+  if (req.body.username && req.body.password) {
 
-  router.post("/login", (req, res) => {
-    if (req.body.username && req.body.password) {
-      console.log(req.body.username);
-      Roaster.findOne({ username: req.body.username }, (error, roaster) => {
-        if (error) console.log(error);
-        if (roaster) {
-          console.log("Found roaster. Checking password...");
-          if (bcrypt.compareSync(req.body.password, roaster.password)) {
-            console.log("Password correct, generating JWT...");
+    // Hash the password:
+    req.body.password = bcrypt.hashSync(
+      req.body.password,
+      bcrypt.genSaltSync(10)
+    );
+
+    Roaster.findOne({ username: req.body.username }, (roaster) => {
+      console.log("========findOne=======", roaster);
+      if (!roaster) {
+        console.log("Running create roaster");
+        Roaster.create(req.body, (error, createdRoaster) => {
+          console.log("createdRoaster", createdRoaster);
+          console.log("error", error);
+          if (createdRoaster) {
             let payload = {
-              id: roaster.id,
-              username: roaster.username
+              id: createdRoaster.id,
             };
+            console.log(payload);
             let token = jwt.encode(payload, config.jwtSecret);
             console.log(token);
             res.json({
               token: token,
             });
           } else {
-            console.log("Wrong password");
+            console.log("failed to create roaster");
             res.sendStatus(401);
           }
+        });
+      } else {
+        console.log("Roaster already exists, try logging in instead");
+        res.sendStatus(401);
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+router.post("/login", (req, res) => {
+  if (req.body.username && req.body.password) {
+    console.log(req.body.username);
+    Roaster.findOne({ username: req.body.username }, (error, roaster) => {
+      if (error) console.log(error);
+      if (roaster) {
+        console.log("Found roaster. Checking password...");
+        if (bcrypt.compareSync(req.body.password, roaster.password)) {
+          console.log("Password correct, generating JWT...");
+          let payload = {
+            id: roaster.id,
+            username: roaster.username
+          };
+          let token = jwt.encode(payload, config.jwtSecret);
+          console.log(token);
+          res.json({
+            token: token,
+            id: roaster._id,
+          });
         } else {
-          console.log("Couldn't find roaster. Try signing up.");
+          console.log("Wrong password");
           res.sendStatus(401);
         }
-      });
-    } else {
-      res.sendStatus(401);
-    }
+      } else {
+        console.log("Couldn't find roaster. Try signing up.");
+        res.sendStatus(401);
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+router.get('/:id', (req, res) => {
+  Roaster.findById(req.params.id, (error, foundRoaster) => {
+      error ? res.status(404).json(error) : res.status(200).json({username: foundRoaster.username,
+      _id: foundRoaster.id});
+
   });
+})
+
+
 
 module.exports = router
